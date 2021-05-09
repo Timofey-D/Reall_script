cfad48d0659d90c14ed07af2f06d2c665ac687a9
Timofey-D <Timofey.Dankevich@gmail.com>
2021-05-09 19:06:38 +0300

    Optimazed a code

--- rename_all.py
+++ rename_all.py
@@ -21,27 +21,27 @@ def rename_files(list_new_name, list_old_name):
             list_new_name.append(new_name)
 
 
+def print_list(_list):
+    for ind in range(0, len(_list)):
+        print(_list[ind], ', ' if len(_list) != ind + 1 else '', sep='', end='')
+    print()
+
+
 def print_changes(list_new_name, list_old_name, rename_what):
     the_first_warning = "The " + rename_what + " was renamed"
     the_second_warning = "The new " + rename_what
     if list_new_name.__len__() != 1 or list_old_name.__len__() != 1:
-        if rename_what == 'directory':
-            rename_what = 'directories'
-        else:
-            rename_what = 'files'
+        rename_what = 'directories' if rename_what == 'directory' else 'files'
+
         the_first_warning = "The " + rename_what + " were renamed"
         the_second_warning = "The new " + rename_what
 
     if list_new_name.__len__() != 0 or list_old_name.__len__() != 0:
         print(the_first_warning, end=': ')
-        for ind in range(0, len(list_old_name)):
-            print(list_old_name[ind], ', ' if len(list_old_name) != ind + 1 else '', sep='', end='')
+        print_list(list_old_name)
 
-        print('\n', the_second_warning, sep='', end=': ')                
-
-        for ind in range(0, len(list_new_name)):
-            print(list_new_name[ind], ', ' if len(list_new_name) != ind + 1 else '', sep='', end='')
-        print()
+        print(the_second_warning, end=': ')                
+        print_list(list_new_name)
     
 
 def __main__():
